9c060a33b82ee52a1d07c6cd1723d0113053122b
Alyssa <add0025@uah.edu>
2020-11-25 20:23:13 -0600

    Captured: Added first option to prompts

--- first_story_line.lua
+++ first_story_line.lua
@@ -4,9 +4,14 @@ local csv = require('csv')
 local csv_path = system.pathForFile ('cursed_story_line_1.csv')
 local f = csv.open(csv_path )
 header = true
-local promptTable = {};
-local counter = 0
- 
+local promptTable = {}; -- table to hold all the Prompts
+local optionsTable = {}; --table that holds all the options
+local cardGroup = display.newGroup() --hold each indiviual card
+local counter = 1
+local prompt_counter = 1
+ local option_counter = 2
+ local card_counter =1
+
 ---------------------------------------------------------------------------------
 -- All code outside of the listener functions will only be executed ONCE
 -- unless "composer.removeScene()" is called.
@@ -20,48 +25,83 @@ local counter = 0
 function scene:create( event )
  
    local sceneGroup = self.view
+
+   --load the prompts from the csv file and put into table
  
    for field  in f:lines() do
 
-      if field[1] == 'Prompts' then
+      if field[1] == 'Prompts' and field[2] == 'Option 1' then
          local promptHeader = field[1]
+         local option1Header = field[2]
+         print(promptHeader)
+         print(option1Header)
       else
       local prompt  =display.newText(field[1], display.contentCenterX, 5, 200, 0, native.systemFont,20)
-      table.insert( promptTable ,  prompt )
+      local option1 =display.newText(field[2], display.contentCenterX, 300, 200, 0, native.systemFont,20)
+
+     -- print (prompt.text)
+     -- print (option1.text)
+     cardGroup:insert(prompt)
+     cardGroup:insert(option1)
+      table.insert( promptTable ,  cardGroup )
       end --end of if statement
      -- print(field[1])
   
   end --end of for loop
 
+
+  --lable the prompts within the table
    
 
-for _, aPrompt in ipairs(promptTable) do
-counter = counter + 1
-aPrompt.name = "Prompt " .. counter .. "a";
-print(aPrompt.name)
+  for _, aPrompt in ipairs(promptTable) do
+   aPrompt[counter].name = "Card " .. card_counter .. "a";
+   --print(aPrompt[counter].name)
+   --print(aPrompt[counter].text)
+   counter = counter + 1
+  aPrompt[counter].name = "Card " .. card_counter .. "a";
+  --print(aPrompt[counter].name)
+   --print (aPrompt[counter].text)
+   counter = counter + 1
+   card_counter =card_counter+1
 
-end --end of for loop
+   end --end of for loop
+   print(counter)
+   counter = 1
 
+   ----function BeginStory()
 
-function BeginStory()
-
-   for _, aPrompt in ipairs(promptTable) do
-
-      if aPrompt.name ~= "Prompt 1a" then
-
-
- aPrompt.isVisible = false
-
+      for _, aPrompt in ipairs(promptTable) do
+      if (aPrompt[counter].name == "Card 1a") then
+      print(aPrompt[counter].text)
       end --end of if
+      if (aPrompt[counter].name ~= "Card 1a") then
+         aPrompt[counter].isVisible = false
+         end --of if
+      counter = counter + 1
+      if (aPrompt[counter].name == "Card 1a") then
+      print(aPrompt[counter].text)
+      end --end of if
+      if (aPrompt[counter].name ~= "Card 1a") then
+         aPrompt[counter].isVisible = false
+         end --of if
+      counter = counter + 1
+
+
+
 
 
    end --end of for loop
+   print(counter)
 
 
+--end --end of BeginStory funciton
 
-end --end of BeginStory funciton
+--Runtime:addEventListener( "enterFrame", BeginStory, 1)
 
-Runtime:addEventListener( "enterFrame", BeginStory, 1)
+
+--load in the options
+
+
    
    -- Initialize the scene here.
    -- Example: add display objects to "sceneGroup", add touch listeners, etc.
